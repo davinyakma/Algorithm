8d58ee5fea58bc616b07d7fe614e637db70ea479
davinyakma <112144319+davinyakma@users.noreply.github.com>
2024-03-29 22:06:40 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char str[1000] = "";
+	char first, last;
+	int n, i, len = 0;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%s", str);
+		len = strlen(str);
+		first = str[0];
+		last = str[len - 1];
+		printf("%c%c\n", first, last);
+	}
+}
